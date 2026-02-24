d11f068a7a4d460c83e3e713ffe66ffebca6c8fe
João <jv09032007@gmail.com>
2026-02-24 15:54:55 -0400

    feat: initial console calculator version

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,46 @@
+﻿namespace Calculator
+{
+    internal static class Calculator
+    {
+        public static double Result { get; private set; }
+
+        public static void Initialization(double num)
+        {
+            Result = num;
+        }
+
+        public static void Clear()
+        {
+            Result = 0;
+        }
+
+        public static void Addition(double num)
+        {
+            Result += num;
+        }
+
+        public static void Subtraction(double num)
+        {
+            Result -= num;
+        }
+
+        public static void Multiplication(double num)
+        {
+            Result *= num;
+        }
+
+        public static void Division(double num)
+        {
+            if(num == 0)
+            {
+                Console.WriteLine("\nDivision by 0 not possible!");
+                Result = -1;
+                return;
+            }
+            else
+            {
+                Result /= num;
+            }
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool calculatorRunning = true;
+
+            while (calculatorRunning)
+            {
+                double num = 0;
+                Console.Write("Insert a number (0 to exit): ");
+
+                while (!double.TryParse(Console.ReadLine(), out num))
+                {
+                    Console.WriteLine("\nInvalid input!\n");
+                }
+
+                if (num == 0)
+                {
+                    break;
+                }
+
+                Calculator.Initialization(num);
+
+                while (true)
+                {
+                    char operation;
+                    Console.WriteLine("\n[+] Addition\n[-] Subtraction\n[*] Multiplication\n[/] Division\n[c] Clear\n[e] Exit\n");
+
+                    while(!char.TryParse(Console.ReadLine(), out operation))
+                    {
+                        Console.WriteLine("Invalid operation!");
+                    }
+
+                    if (operation == 'e')
+                    {
+                        calculatorRunning = false;
+                        break;
+                    }
+
+                    if (operation == 'c')
+                    {
+                        Calculator.Clear();
+                        Console.WriteLine($"\nResult: {Calculator.Result}\n");
+                        continue;
+                    }
+
+                    Console.Write("\nInsert another number: ");
+                    while (!double.TryParse(Console.ReadLine(), out num))
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+
+                    switch (operation)
+                    {
+                        case '+':
+                            Calculator.Addition(num);
+                            break;
+                        case '-':
+                            Calculator.Subtraction(num);
+                            break;
+                        case '*':
+                            Calculator.Multiplication(num);
+                            break;
+                        case '/':
+                            Calculator.Division(num);
+                            break;
+                        default:
+                            Console.WriteLine("\nInvalid operation!");
+                            Calculator.Clear();
+                            break;
+                    }
+                    Console.WriteLine($"\nResult: {Calculator.Result}\n");
+                }
+            }
+        }
+    }
+}
